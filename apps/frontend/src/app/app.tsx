8334454bc92050e3afcf046b5c007ee243739a45
Ahmed <a7med3365@gmail.com>
2024-11-09 18:57:56 +0300

    Add Navbar and Sidebar components with Catalyst UI Kit integration

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,9 +1,85 @@
-import NxWelcome from './nx-welcome';
+import Card from '../components/card';
+import { SidebarLayout } from '../components/catalyst-ui-kit/sidebar-layout';
+import NavbarComponent from '../components/layout/navbar';
+import SidebarComponent from '../components/layout/sidebar';
+import Section from '../components/section';
+
+const movies = [
+  {
+    imageUrl:
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fqldf2t8ztc9aiwn3k6mlX3tvRT.jpg',
+    title: 'Arcane',
+    year: '2024',
+    rating: '7.5',
+  },
+  {
+    imageUrl:
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/peNC0eyc3TQJa6x4TdKcBPNP4t0.jpg',
+    title: 'The Matrix Resurrections',
+    year: '2021',
+    rating: '7.5',
+  },
+  {
+    imageUrl:
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/AgBNLcHFEXCRFZuKv0H8RWMxNAJ.jpg',
+    title: 'Don’t Look Up',
+    year: '2021',
+    rating: '7.5',
+  },
+  {
+    imageUrl:
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5bFK5d3mVTAvBCXi5NPWH0tYjKl.jpg',
+    title: 'Spider Man: No Way Home',
+    year: '2021',
+    rating: '7.5',
+  },
+  {
+    imageUrl:
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fqldf2t8ztc9aiwn3k6mlX3tvRT.jpg',
+    title: 'Arcane',
+    year: '2024',
+    rating: '7.5',
+  },
+  {
+    imageUrl:
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/peNC0eyc3TQJa6x4TdKcBPNP4t0.jpg',
+    title: 'The Matrix Resurrections',
+    year: '2021',
+    rating: '7.5',
+  },
+  {
+    imageUrl:
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/AgBNLcHFEXCRFZuKv0H8RWMxNAJ.jpg',
+    title: 'Don’t Look Up',
+    year: '2021',
+    rating: '7.5',
+  },
+  {
+    imageUrl:
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5bFK5d3mVTAvBCXi5NPWH0tYjKl.jpg',
+    title: 'Spider Man: No Way Home',
+    year: '2021',
+    rating: '7.5',
+  },
+];
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="frontend" />
+      <SidebarLayout
+        navbar={<NavbarComponent />}
+        sidebar={<SidebarComponent />}
+      >
+        <Section movies={movies} header="Popular Movies" />
+        <Section movies={movies} header="Popular TV Shows" />
+        <Section movies={movies} header="Popular Posts" />
+        {/* <Card
+          title="arcane"
+          year="2024"
+          imageUrl="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fqldf2t8ztc9aiwn3k6mlX3tvRT.jpg"
+          rating="7.5"
+        /> */}
+      </SidebarLayout>
     </div>
   );
 }
---
+++ apps/frontend/src/components/layout/sidebar.tsx
@@ -0,0 +1,143 @@
+import { Avatar } from '../catalyst-ui-kit/avatar';
+import {
+  Dropdown,
+  DropdownButton,
+  DropdownDivider,
+  DropdownItem,
+  DropdownLabel,
+  DropdownMenu,
+} from '../catalyst-ui-kit/dropdown';
+import {
+  Sidebar,
+  SidebarBody,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarHeading,
+  SidebarItem,
+  SidebarLabel,
+  SidebarSection,
+  SidebarSpacer,
+} from '../catalyst-ui-kit/sidebar';
+import {
+  ArrowRightStartOnRectangleIcon,
+  ChatBubbleBottomCenterTextIcon,
+  ChevronUpIcon,
+  Cog8ToothIcon,
+  FilmIcon,
+  HomeIcon,
+  LightBulbIcon,
+  MagnifyingGlassIcon,
+  QuestionMarkCircleIcon,
+  ShieldCheckIcon,
+  StarIcon,
+  TvIcon,
+  UserIcon,
+} from '@heroicons/react/16/solid';
+import movieClapperboard from '../../assets/movie-clapperboard.svg';
+
+const SidebarComponent = () => (
+  <Sidebar>
+    <SidebarHeader>
+      <SidebarSection className="max-lg:hidden">
+        <SidebarItem href="/search">
+        <Avatar src={movieClapperboard} />
+        <SidebarLabel>Tailwind Labs</SidebarLabel>
+        {/* <ChevronDownIcon /> */}
+        </SidebarItem>
+      </SidebarSection>
+      <SidebarSection className="max-lg:hidden">
+        <SidebarItem href="/search">
+          <MagnifyingGlassIcon />
+          <SidebarLabel>Search</SidebarLabel>
+        </SidebarItem>
+        <SidebarItem href="/inbox">
+          <StarIcon />
+          <SidebarLabel>Favorites</SidebarLabel>
+        </SidebarItem>
+      </SidebarSection>
+    </SidebarHeader>
+    <SidebarBody>
+      <SidebarSection>
+        <SidebarItem href="/">
+          <HomeIcon />
+          <SidebarLabel>Home</SidebarLabel>
+        </SidebarItem>
+        <SidebarItem href="/events">
+          <FilmIcon />
+          <SidebarLabel>Movies</SidebarLabel>
+        </SidebarItem>
+        <SidebarItem href="/orders">
+          <TvIcon />
+          <SidebarLabel>TV Shows</SidebarLabel>
+        </SidebarItem>
+        <SidebarItem href="/Posts">
+          <ChatBubbleBottomCenterTextIcon />
+          <SidebarLabel>Posts</SidebarLabel>
+        </SidebarItem>
+      </SidebarSection>
+      <SidebarSection className="max-lg:hidden">
+        <SidebarHeading>Featured stuff</SidebarHeading>
+        <SidebarItem href="/events/1">Bear Hug: Live in Concert</SidebarItem>
+        <SidebarItem href="/events/2">Viking People</SidebarItem>
+        <SidebarItem href="/events/3">Six Fingers — DJ Set</SidebarItem>
+        <SidebarItem href="/events/4">We All Look The Same</SidebarItem>
+      </SidebarSection>
+      <SidebarSpacer />
+      <SidebarSection>
+        <SidebarItem href="/support">
+          <QuestionMarkCircleIcon />
+          <SidebarLabel>Support</SidebarLabel>
+        </SidebarItem>
+      </SidebarSection>
+    </SidebarBody>
+    <SidebarFooter className="max-lg:hidden">
+      <Dropdown>
+        <DropdownButton as={SidebarItem}>
+          <span className="flex min-w-0 items-center gap-3">
+            <Avatar
+              src="/profile-photo.jpg"
+              className="size-10"
+              square
+              alt=""
+            />
+            <span className="min-w-0">
+              <span className="block truncate text-sm/5 font-medium text-zinc-950 dark:text-white">
+                Erica
+              </span>
+              <span className="block truncate text-xs/5 font-normal text-zinc-500 dark:text-zinc-400">
+                erica@example.com
+              </span>
+            </span>
+          </span>
+          <ChevronUpIcon />
+        </DropdownButton>
+        <DropdownMenu className="min-w-64" anchor="top start">
+          <DropdownItem href="/my-profile">
+            <UserIcon />
+            <DropdownLabel>My profile</DropdownLabel>
+          </DropdownItem>
+          <DropdownItem href="/settings">
+            <Cog8ToothIcon />
+            <DropdownLabel>Settings</DropdownLabel>
+          </DropdownItem>
+          <DropdownDivider />
+          <DropdownItem href="/privacy-policy">
+            <ShieldCheckIcon />
+            <DropdownLabel>Privacy policy</DropdownLabel>
+          </DropdownItem>
+          <DropdownItem href="/share-feedback">
+            <LightBulbIcon />
+            <DropdownLabel>Share feedback</DropdownLabel>
+          </DropdownItem>
+          <DropdownDivider />
+          <DropdownItem href="/logout">
+            <ArrowRightStartOnRectangleIcon />
+            <DropdownLabel>Sign out</DropdownLabel>
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </SidebarFooter>
+  </Sidebar>
+);
+
+export default SidebarComponent;
