c0ec75296f9f6a998957d3164ee1e4c8c5297b2c
Ahmed <a7med3365@gmail.com>
2024-11-11 22:34:58 +0300

    Refactor App component to implement routing with React Router and remove hardcoded movie sections

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -3,84 +3,31 @@ import { SidebarLayout } from '../components/catalyst-ui-kit/sidebar-layout';
 import NavbarComponent from '../components/layout/navbar';
 import SidebarComponent from '../components/layout/sidebar';
 import Section from '../components/section';
-
-const movies = [
-  {
-    imageUrl:
-      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fqldf2t8ztc9aiwn3k6mlX3tvRT.jpg',
-    title: 'Arcane',
-    year: '2024',
-    rating: '7.5',
-  },
-  {
-    imageUrl:
-      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/peNC0eyc3TQJa6x4TdKcBPNP4t0.jpg',
-    title: 'The Matrix Resurrections',
-    year: '2021',
-    rating: '7.5',
-  },
-  {
-    imageUrl:
-      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/AgBNLcHFEXCRFZuKv0H8RWMxNAJ.jpg',
-    title: 'Don’t Look Up',
-    year: '2021',
-    rating: '7.5',
-  },
-  {
-    imageUrl:
-      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5bFK5d3mVTAvBCXi5NPWH0tYjKl.jpg',
-    title: 'Spider Man: No Way Home',
-    year: '2021',
-    rating: '7.5',
-  },
-  {
-    imageUrl:
-      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fqldf2t8ztc9aiwn3k6mlX3tvRT.jpg',
-    title: 'Arcane',
-    year: '2024',
-    rating: '7.5',
-  },
-  {
-    imageUrl:
-      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/peNC0eyc3TQJa6x4TdKcBPNP4t0.jpg',
-    title: 'The Matrix Resurrections',
-    year: '2021',
-    rating: '7.5',
-  },
-  {
-    imageUrl:
-      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/AgBNLcHFEXCRFZuKv0H8RWMxNAJ.jpg',
-    title: 'Don’t Look Up',
-    year: '2021',
-    rating: '7.5',
-  },
-  {
-    imageUrl:
-      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5bFK5d3mVTAvBCXi5NPWH0tYjKl.jpg',
-    title: 'Spider Man: No Way Home',
-    year: '2021',
-    rating: '7.5',
-  },
-];
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from '../pages/home';
+import MoviesPage from '../pages/movies';
+import TvPage from '../pages/tv';
+import PostsPage from '../pages/posts';
+import SignIn from '../pages/signin';
+import Details from '../pages/details';
 
 export function App() {
   return (
-    <div>
+    <Router>
       <SidebarLayout
         navbar={<NavbarComponent />}
         sidebar={<SidebarComponent />}
       >
-        <Section movies={movies} header="Popular Movies" />
-        <Section movies={movies} header="Popular TV Shows" />
-        <Section movies={movies} header="Popular Posts" />
-        {/* <Card
-          title="arcane"
-          year="2024"
-          imageUrl="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fqldf2t8ztc9aiwn3k6mlX3tvRT.jpg"
-          rating="7.5"
-        /> */}
+        <Routes>
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/tv" element={<TvPage />} />
+          <Route path="/posts" element={<PostsPage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
       </SidebarLayout>
-    </div>
+    </Router>
   );
 }
 
