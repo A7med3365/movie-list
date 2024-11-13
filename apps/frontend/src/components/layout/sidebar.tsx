901d6f63833687828c695a4ab0e1dcc356d3b67e
Ahmed <a7med3365@gmail.com>
2024-11-13 17:25:48 +0300

    Refactor SidebarComponent to integrate authentication context and enhance user experience with dynamic avatar display and sign-out functionality

--- apps/frontend/src/components/layout/sidebar.tsx
+++ apps/frontend/src/components/layout/sidebar.tsx
@@ -34,110 +34,148 @@ import {
   UserIcon,
 } from '@heroicons/react/16/solid';
 import movieClapperboard from '../../assets/movie-clapperboard.svg';
+import { useContext, useEffect } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+import { Button } from '../catalyst-ui-kit/button';
+import { avatars } from '../../assets/avatar/avatar';
+import useRequest from '../../hooks/useRequest';
+import { useNavigate } from 'react-router-dom';
 
-const SidebarComponent = () => (
-  <Sidebar>
-    <SidebarHeader>
-      <SidebarSection className="max-lg:hidden">
-        <SidebarItem href="/search">
-          <Avatar src={movieClapperboard} />
-          <SidebarLabel>Tailwind Labs</SidebarLabel>
-          {/* <ChevronDownIcon /> */}
-        </SidebarItem>
-      </SidebarSection>
-      <SidebarSection className="max-lg:hidden">
-        <SidebarItem href="/search">
-          <MagnifyingGlassIcon />
-          <SidebarLabel>Search</SidebarLabel>
-        </SidebarItem>
-        <SidebarItem href="/inbox">
-          <StarIcon />
-          <SidebarLabel>Favorites</SidebarLabel>
-        </SidebarItem>
-      </SidebarSection>
-    </SidebarHeader>
-    <SidebarBody>
-      <SidebarSection>
-        <SidebarItem href="/">
-          <HomeIcon />
-          <SidebarLabel>Home</SidebarLabel>
-        </SidebarItem>
-        <SidebarItem href="/movies">
-          <FilmIcon />
-          <SidebarLabel>Movies</SidebarLabel>
-        </SidebarItem>
-        <SidebarItem href="/tv">
-          <TvIcon />
-          <SidebarLabel>TV Shows</SidebarLabel>
-        </SidebarItem>
-        <SidebarItem href="/posts">
-          <ChatBubbleBottomCenterTextIcon />
-          <SidebarLabel>Posts</SidebarLabel>
-        </SidebarItem>
-      </SidebarSection>
-      <SidebarSection className="max-lg:hidden">
-        <SidebarHeading>Featured stuff</SidebarHeading>
-        <SidebarItem href="/details/194764?type=tv">The Penguin</SidebarItem>
-        <SidebarItem href="/details/1396?type=tv">Breaking Bad</SidebarItem>
-        <SidebarItem href="/details/94605?type=tv">Arcane</SidebarItem>
-        <SidebarItem href="/details/88803?type=tv">Vinland Saga</SidebarItem>
-      </SidebarSection>
-      <SidebarSpacer />
-      <SidebarSection>
-        <SidebarItem href="/support">
-          <QuestionMarkCircleIcon />
-          <SidebarLabel>Support</SidebarLabel>
-        </SidebarItem>
-      </SidebarSection>
-    </SidebarBody>
-    <SidebarFooter className="max-lg:hidden">
-      <Dropdown>
-        <DropdownButton as={SidebarItem}>
-          <span className="flex min-w-0 items-center gap-3">
-            <Avatar
-              src="/profile-photo.jpg"
-              className="size-10"
-              square
-              alt=""
-            />
-            <span className="min-w-0">
-              <span className="block truncate text-sm/5 font-medium text-zinc-950 dark:text-white">
-                Erica
+const SidebarComponent = () => {
+  const { isSignedIn, currentUser, signOut } = useContext(AuthContext);
+  const nav = useNavigate();
+  const { doRequest: callSignOut } = useRequest({
+    url: '/api/auth/signout',
+    method: 'post',
+    onSuccess: () => {
+      signOut();
+      nav('/');
+    },
+  });
+
+  useEffect(() => {
+    // console.log(currentUser);
+  }, [currentUser]);
+
+  useEffect(() => {
+    // console.log(isSignedIn);
+  }, [isSignedIn]);
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <SidebarSection className="max-lg:hidden">
+          <SidebarItem href="/">
+            <Avatar src={movieClapperboard} />
+            <SidebarLabel>TMDB</SidebarLabel>
+            {/* <ChevronDownIcon /> */}
+          </SidebarItem>
+        </SidebarSection>
+        <SidebarSection className="max-lg:hidden">
+          <SidebarItem href="/search">
+            <MagnifyingGlassIcon />
+            <SidebarLabel>Search</SidebarLabel>
+          </SidebarItem>
+          <SidebarItem href="/inbox">
+            <StarIcon />
+            <SidebarLabel>Favorites</SidebarLabel>
+          </SidebarItem>
+        </SidebarSection>
+      </SidebarHeader>
+      <SidebarBody>
+        <SidebarSection>
+          <SidebarItem href="/">
+            <HomeIcon />
+            <SidebarLabel>Home</SidebarLabel>
+          </SidebarItem>
+          <SidebarItem href="/movies">
+            <FilmIcon />
+            <SidebarLabel>Movies</SidebarLabel>
+          </SidebarItem>
+          <SidebarItem href="/tv">
+            <TvIcon />
+            <SidebarLabel>TV Shows</SidebarLabel>
+          </SidebarItem>
+          <SidebarItem href="/posts">
+            <ChatBubbleBottomCenterTextIcon />
+            <SidebarLabel>Posts</SidebarLabel>
+          </SidebarItem>
+        </SidebarSection>
+        <SidebarSection className="max-lg:hidden">
+          <SidebarHeading>Featured stuff</SidebarHeading>
+          <SidebarItem href="/details/194764?type=tv">The Penguin</SidebarItem>
+          <SidebarItem href="/details/1396?type=tv">Breaking Bad</SidebarItem>
+          <SidebarItem href="/details/94605?type=tv">Arcane</SidebarItem>
+          <SidebarItem href="/details/88803?type=tv">Vinland Saga</SidebarItem>
+        </SidebarSection>
+        <SidebarSpacer />
+        <SidebarSection>
+          <SidebarItem>
+            <QuestionMarkCircleIcon />
+            <SidebarLabel>Support</SidebarLabel>
+          </SidebarItem>
+        </SidebarSection>
+      </SidebarBody>
+      <SidebarFooter className="max-lg:hidden">
+        {isSignedIn ? (
+          <Dropdown>
+            <DropdownButton as={SidebarItem}>
+              <span className="flex min-w-0 items-center gap-3">
+                <Avatar
+                  src={
+                    currentUser?.avatarIndex !== undefined
+                      ? avatars[currentUser.avatarIndex]
+                      : avatars[0]
+                  }
+                  className="size-10"
+                  square
+                  alt=""
+                />
+                <span className="min-w-0">
+                  <span className="block truncate text-sm/5 font-medium text-zinc-950 dark:text-white">
+                    {currentUser?.username}
+                  </span>
+                  <span className="block truncate text-xs/5 font-normal text-zinc-500 dark:text-zinc-400">
+                    {currentUser?.email}
+                  </span>
+                </span>
               </span>
-              <span className="block truncate text-xs/5 font-normal text-zinc-500 dark:text-zinc-400">
-                erica@example.com
-              </span>
-            </span>
-          </span>
-          <ChevronUpIcon />
-        </DropdownButton>
-        <DropdownMenu className="min-w-64" anchor="top start">
-          <DropdownItem href="/my-profile">
-            <UserIcon />
-            <DropdownLabel>My profile</DropdownLabel>
-          </DropdownItem>
-          <DropdownItem href="/settings">
-            <Cog8ToothIcon />
-            <DropdownLabel>Settings</DropdownLabel>
-          </DropdownItem>
-          <DropdownDivider />
-          <DropdownItem href="/privacy-policy">
-            <ShieldCheckIcon />
-            <DropdownLabel>Privacy policy</DropdownLabel>
-          </DropdownItem>
-          <DropdownItem href="/share-feedback">
-            <LightBulbIcon />
-            <DropdownLabel>Share feedback</DropdownLabel>
-          </DropdownItem>
-          <DropdownDivider />
-          <DropdownItem href="/logout">
-            <ArrowRightStartOnRectangleIcon />
-            <DropdownLabel>Sign out</DropdownLabel>
-          </DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-    </SidebarFooter>
-  </Sidebar>
-);
+              <ChevronUpIcon />
+            </DropdownButton>
+            <DropdownMenu className="min-w-64" anchor="top start">
+              <DropdownItem>
+                <UserIcon />
+                <DropdownLabel>My profile</DropdownLabel>
+              </DropdownItem>
+              <DropdownItem>
+                <Cog8ToothIcon />
+                <DropdownLabel>Settings</DropdownLabel>
+              </DropdownItem>
+              <DropdownDivider />
+              <DropdownItem>
+                <ShieldCheckIcon />
+                <DropdownLabel>Privacy policy</DropdownLabel>
+              </DropdownItem>
+              <DropdownItem>
+                <LightBulbIcon />
+                <DropdownLabel>Share feedback</DropdownLabel>
+              </DropdownItem>
+              <DropdownDivider />
+              <DropdownItem onClick={() => callSignOut()}>
+                <ArrowRightStartOnRectangleIcon />
+                <DropdownLabel>Sign out</DropdownLabel>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        ) : (
+          <SidebarItem href="/signin">
+            <Button color="indigo" style={{ width: '100%' }}>
+              Sign in
+            </Button>
+          </SidebarItem>
+        )}
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
 
 export default SidebarComponent;
