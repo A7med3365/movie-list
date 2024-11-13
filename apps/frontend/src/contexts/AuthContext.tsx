9e024e226228ae9b080444cb77390f65ee6bf1d9
Ahmed <a7med3365@gmail.com>
2024-11-13 17:25:10 +0300

    Add AuthContext for user authentication management

---
+++ apps/frontend/src/contexts/AuthContext.tsx
@@ -0,0 +1,57 @@
+import { createContext, useState } from 'react';
+import { ReactNode } from 'react';
+import useRequest from '../hooks/useRequest';
+import { useEffect } from 'react';
+
+interface user {
+  id: string;
+  email: string;
+  username: string;
+  avatarIndex: number;
+  //   favorites: string[];
+  //   posts: string[];
+}
+
+interface AuthContextType {
+  isSignedIn: boolean;
+  signIn: () => void;
+  signOut: () => void;
+  currentUser: user | null;
+}
+
+export const AuthContext = createContext<AuthContextType>({
+  isSignedIn: false,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  signIn: () => {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  signOut: () => {},
+  currentUser: null,
+});
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState<user | null>(null);
+
+  const { doRequest: checkCurrentUser } = useRequest({
+    url: 'api/auth/currentuser',
+    method: 'get',
+    onSuccess: (res) => {
+      setIsSignedIn(true);
+      setCurrentUser(res.data);
+    },
+    onError: () => setIsSignedIn(false),
+  });
+
+  useEffect(() => {
+    checkCurrentUser();
+  }, []);
+
+  const value = {
+    isSignedIn,
+    signIn: () => setIsSignedIn(true),
+    signOut: () => setIsSignedIn(false),
+    currentUser,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
